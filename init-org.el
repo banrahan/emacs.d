5515aef2a52876646de1a828e17a63b659284a07
Ben Hanrahan <ben.hanrahan@xrce.xerox.com>
2013-08-30 17:59:09 +0200

    recursively find org files in my working directory

--- init-org.el
+++ init-org.el
@@ -1,6 +1,14 @@
 ;;; org mode
 ;; The following lines are always needed. Choose your own keys.
 (require 'org)
+
+;; recursively add org files
+(load-library "find-lisp")
+;; from working
+(setq org-agenda-files (find-lisp-find-files "~/working" "\.org$"))
+
+;; from projects
+
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
 (global-set-key (kbd "C-c l") 'org-store-link)
