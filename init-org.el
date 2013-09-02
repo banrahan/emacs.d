ef2ae7321b7388fa795204ad91ee89fe8ac4a9ec
Ben Hanrahan <ben.hanrahan@xrce.xerox.com>
2013-09-02 17:07:33 +0200

    added quick capture for org mode

--- init-org.el
+++ init-org.el
@@ -4,17 +4,21 @@
 
 ;; recursively add org files
 (load-library "find-lisp")
+
 ;; from working
 (setq org-agenda-files (find-lisp-find-files "~/working" "\.org$"))
 
 ;; from projects
-
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+
+;; some key bindings  
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c b") 'org-iswitchb)
 
+;; custom view for what is due today
 (setq org-agenda-custom-commands
       '(("I" "Immediate"
          ((agenda "" ((org-agenda-ndays 1))) ;; limits the agenda display to a single day
@@ -26,8 +30,12 @@
        ))
 )
 
+;; setup 0-9 priorities
 (setq org-default-priority 57)
 (setq org-highest-priority 48)
 (setq org-lowest-priority 57)
 
+;; setup capture
+(setq org-default-notes-file "~/working/org/inbox.org")
+(define-key global-map "\C-cc" 'org-capture)
 (provide 'init-org)
