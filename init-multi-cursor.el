9985c7cb9f8f1987d9f00ee8ff0b59393947fca3
Ben Hanrahan <hanrahan.ben@gmail.com>
2013-09-07 07:58:10 +0200

    clean up

--- init-multi-cursor.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'load-path "~/.emacs/site-elisp/multiple-cursors")
-(require 'multiple-cursors)
-
-(global-set-key (kbd "s-d") 'mc/mark-next-like-this)
-(global-set-key (kbd "s-D") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-d") 'mc/mark-all-like-this)
-
-(provide 'init-multi-cursor)
