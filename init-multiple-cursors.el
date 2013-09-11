745a0dea5927942c45ab5291aab022ab0af720e2
Ben Hanrahan <hanrahan.ben@gmail.com>
2013-09-11 07:49:39 +0200

    whoops, correcting typo

--- init-multiple-cursors.el
+++ init-multiple-cursors.el
@@ -1,8 +1,7 @@
 (require 'multiple-cursors)
 (global-set-key (kbd "s-d") 'mc/mark-next-like-this)
 (global-set-key (kbd "s-D") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-d") 'mc/mark-all-like-dwim)
+(global-set-key (kbd "C-c C-d") 'mc/mark-all-like-this-dwim)
 (global-set-key (kbd "s-<mouse-1>") 'mc/add-cursor-on-click)
-
 
 (provide 'init-multiple-cursors)
