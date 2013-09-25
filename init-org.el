3b2bc8bc92eafa5b0ecb07674b2f21f4865f7f90
Ben Hanrahan <ben.hanrahan@xrce.xerox.com>
2013-09-25 11:30:56 +0200

    mobile org support

--- init-org.el
+++ init-org.el
@@ -50,5 +50,32 @@
 (setq org-refile-targets (quote ((nil :maxlevel . 1)
                                  (org-agenda-files :maxlevel . 1))))
 
+; mobile-org
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/inbox.org")
+(setq org-mobile-files (quote ("~/Dropbox/org")))
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(setq org-mobile-capture-file "~/Dropbox/Apps/MobileOrg/mobileorg.org")
+(setq org-directory "~/Dropbox/org")
 
+; mobile-org pushing
+(defvar org-mobile-push-timer nil
+  "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
+
+(defun org-mobile-push-with-delay (secs)
+  (when org-mobile-push-timer
+    (cancel-timer org-mobile-push-timer))
+  (setq org-mobile-push-timer
+        (run-with-idle-timer
+         (* 1 secs) nil 'org-mobile-push)))
+
+(add-hook 'after-save-hook 
+ (lambda () 
+   (when (eq major-mode 'org-mode)
+     (dolist (file (org-mobile-files-alist))
+      (if (string= (file-truename (expand-file-name (car file)))
+		   (file-truename (buffer-file-name)))
+           (org-mobile-push-with-delay 30)))
+   )))
+
+(run-at-time "00:05" 86400 '(lambda () (org-mobile-push-with-delay 1))) ;; refreshes agenda file each day
 (provide 'init-org)
