b9013f60df14bfe220462d320e06d28d0d145385
Ben Hanrahan <hanrahan.ben@gmail.com>
2014-03-13 20:32:51 +0100

    put in comments and stuff for autopair

--- init-autopair.el
+++ init-autopair.el
@@ -1,4 +1,14 @@
+;;; init-autopair.el --- Autopair configuration
+;;
+;;; Commentary:
+;;
+;; global autopair
+;;
+
+;;; Code:
 (require 'autopair)
 (autopair-global-mode) ;; enable autopair in all buffers
 
 (provide 'init-autopair)
+
+;;; init-autopair.el ends here
