99c430a25e66c2fcbde693bf4064b66bb9a03dbd
banrahan <hanrahan.ben@gmail.com>
2014-03-27 18:57:38 +0100

    airline and some other stuff

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 ;;
 ;; Each thing is divided up into sections, use helm-imenu to list them
-;;   helm-imenu is bound to <s-r> in this buffer, or <leader>-p globally for this buffer
+;;   helm-imenu is bound to <s-r> in this buffer
 ;;
 
 ;;; Code:
@@ -69,6 +69,14 @@
 (require 'helm-projectile)
 (require 'powerline)
 
+(require 'diminish)
+(eval-after-load "yasnippet" '(diminish 'yas-minor-mode " Y"))
+(eval-after-load "Undo-Tree" '(diminish 'undo-tree-mode))
+(eval-after-load "abbrev" '(diminish 'abbrev-mode "Ab"))
+(eval-after-load "autopair" '(diminish 'autopair-mode))
+(eval-after-load "magit" '(diminish 'magit-auto-revert-mode))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Evil
 ;;;;;;;;;;;;;;;;;;;;;;
@@ -124,8 +132,6 @@
     )
   )
 
-;; powerline
-(powerline-vim-theme)
 
 ;; tabs
 (elscreen-start)
@@ -144,6 +150,63 @@
                     :background "#073642"
                     :foreground "#93A1A1"
                     )
+
+;;;;;;;;;;;;;;;;;;;;;;
+;;;; powerline
+;;;;;;;;;;;;;;;;;;;;;;
+(defun powerline-banrahan-theme ()
+  "Setup a banrahan mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          powerline-default-separator
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           powerline-default-separator
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list
+
+                                     (powerline-raw evil-mode-line-tag mode-line 'l)
+                                     (powerline-buffer-id `(mode-line-buffer-id ,mode-line) 'l)
+                                     (powerline-raw "[" mode-line 'l)
+                                     (powerline-major-mode mode-line)
+                                     (powerline-process mode-line)
+                                     (powerline-raw "]" mode-line)
+                                     (when (buffer-modified-p)
+                                       (powerline-raw "[+]" mode-line))
+                                     (when buffer-read-only
+                                       (powerline-raw "[RO]" mode-line))
+                                     (powerline-raw "[%z]" mode-line)
+                                     ;; (powerline-raw (concat "[" (mode-line-eol-desc) "]") mode-line)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format nil 'l))
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-raw "[" mode-line 'l)
+                                     (powerline-minor-modes mode-line)
+                                     (powerline-raw "%n" mode-line)
+                                     (powerline-raw "]" mode-line)
+                                     (when (and vc-mode buffer-file-name)
+                                       (let ((backend (vc-backend buffer-file-name)))
+                                         (when backend
+                                           (concat (powerline-raw "[" mode-line 'l)
+                                                   (powerline-raw (format "%s / %s" backend (vc-working-revision buffer-file-name backend)))
+                                                   (powerline-raw "]" mode-line)))))))
+                          (rhs (list (powerline-raw '(10 "%i"))
+                                     (powerline-raw global-mode-string mode-line 'r)
+                                     (powerline-raw "%l," mode-line 'l)
+                                     (powerline-raw (format-mode-line '(10 "%c")))
+                                     (powerline-raw (replace-regexp-in-string  "%" "%%" (format-mode-line '(-3 "%p"))) mode-line 'r))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill mode-line (powerline-width rhs))
+                             (powerline-render rhs)))))))
+(powerline-banrahan-theme)
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;;; popwin
