5905c23d20a07cb1a72623ad03edf4d7ba7b41dc
Ben Hanrahan <hanrahan.ben@gmail.com>
2014-04-29 17:27:29 +0200

    Clojure support

--- elisp/300_languages.el
+++ elisp/300_languages.el
@@ -5,4 +5,10 @@
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;;;; clojure
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-mode-hook (lambda () (paredit-mode 1)))
+
 (provide '300_languages)
