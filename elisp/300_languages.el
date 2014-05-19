d2177c376750880750e72bcb91bd8de1aa683c39
Ben Hanrahan <hanrahan.ben@gmail.com>
2014-05-19 17:31:21 +0200

    added some javascript support

--- elisp/300_languages.el
+++ elisp/300_languages.el
@@ -11,4 +11,13 @@
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-mode-hook (lambda () (paredit-mode 1)))
 
+;;;; javascript
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+  '(progn
+     (require 'tern-auto-complete)
+     (tern-ac-setup)))
+
+
 (provide '300_languages)
